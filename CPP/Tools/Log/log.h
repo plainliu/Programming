ca15749d808df0ed86a7221487b21dd6b3c8726c
Juanjuan <liujuanjuan134@foxmail.com>
2021-09-11 10:14:49 +0800

    优化可变参的编译期检查

--- CPP/Tools/Log/log.h
+++ CPP/Tools/Log/log.h
@@ -5,6 +5,7 @@
 #include "windows.h"
 #include <iostream>
 
+#include <cassert>
 
 // 核心打印函数
 void PrintHelper( const wchar_t* format, ... )
@@ -20,42 +21,88 @@ void PrintHelper( const wchar_t* format, ... )
 	::OutputDebugString( L"\r\n" );
 };
 
-// 类型检查
+// ==========================================TypeCheker
 template <typename T>
 struct TypeCheker;
 
-template <>
-struct TypeCheker<int> {};
+// 不实现: 编译期报错
+// 实现：运行期assert
+//template <typename T>
+//struct TypeCheker {
+//	TypeCheker( ){
+//		assert(false); // 运行期检查
+//	};
+//};
 
 template <>
-struct TypeCheker<void*> {};
+struct TypeCheker<int> { };
+
+//template <>
+//struct TypeCheker<void*> { };
+
+// ==========================================StaticTypeCheker
+
+template <typename T>
+struct StaticTypeCheker;
+
+template <typename T>
+struct StaticTypeCheker { enum { Support = 0 }; };
+
+template <>
+struct StaticTypeCheker<int> { enum { Support = 1 }; };
+
+//template <>
+//struct StaticTypeCheker<void*> { enum { Support = 1 }; };
 
 // ...
 
-/// <summary>
-/// 目的是让可变参数列表一个一个解析，最后达到所有参数被访问的情况
-/// 主要是为了做可变参数的类型检查
-/// 通过统一接口传入可变参数列表，逐项检查，检查的过程实质是特化的过程
-/// 特化对应不上的，输出asset？不写出来，编译不通过？
-/// </summary>
+// ==========================================Cheker
+// 解析可变参数，为了做可变参数的类型检查
+
 // 终止
 void Checker( ) { }
-//展开函数
+// 展开函数
 template <typename T, typename ...Args>
 void Checker( T head, Args... rest )
 {
-	// 只有debug情况下才需要通过构造TypeCheker来检查类型
-	// TypeCheker<T>; // 检查实质发生在编译期，运行时的构造没有必要（不需要构造，只给个类型）。但这样可以编译通过？
-	TypeCheker<T>( ); // 问题：生成失败，报错的时候不知道是哪个参数格式检查不通过
+	// 1. 构造T类型对象，支持的类型特化，不支持则编译不通过
+	// TypeCheker<T>( ); // 问题：1. 生成失败，报错的时候不知道是哪个参数格式检查不通过 2. 除了编译器做检查，运行期还会调用构造生成对象
+	// TypeCheker<T>; // 检查实质发生在编译期，运行时的构造没有必要（不需要构造，只给个类型）。但这样可以编译通过（有模板声明）
+
+	// 思路
+	// 1. 只有debug情况下才需要通过构造TypeCheker来检查类型
+	// 2. 只有编译期间需要检查类型，运行期间最好什么事情都不发生（如果编译器可以检查，没必要区分Debug）
+	// 3. 能在编译器抛错且能说明原因
+
+	// 2. 编译期间静态检查（利用enum也是编译器就可以识别的）
+	static_assert( StaticTypeCheker<T>::Support == 1, "Unsupport Type!" ); // 做到了只在编译期检查，而且有输出
+
+	// 缺点：报错中没有出错的具体类型
+
 	Checker( rest... );
 }
+
+// ==========================================MyPrintWithTypeCheck
 
 template <typename ...Args>
 void MyPrint( const wchar_t* format, Args... args )
 {
+	//#DEBUG 只在debug开，就不会定义一堆Checker函数了
 	Checker( args... );
+
 	PrintHelper( format, args... );
 }
+
+//void MyPrint( const wchar_t* format, ... )
+//{
+//	va_list alist;
+//	va_start( alist, format );
+//	Checker( alist ); // 只检查了va_list（char*），没有逐项检查
+//	PrintHelper( format, alist );
+//	va_end( alist );
+//}
+
+// ==========================================export macros
 
 #define MYTEXT( x ) L ## x
 #define LJJ_LOG( format, ... ) \
